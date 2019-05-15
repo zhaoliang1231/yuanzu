1bd3ff07f504585a61e70d9061474cdab091af1b
yfTyro <2830843732@qq.com>
2019-05-15 18:20:18 +0800

    .

--- src/Pabulic/Header/Header.vue
+++ src/Pabulic/Header/Header.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="top-nav"></div>
 </template>
-
 <script>
 export default {
   name: 'Header'
 }
 </script>
-
 <style scoped lang="less">
-  @import '../../static/css/common.less';
-  .top-nav{
-    height: 24px;
-    background: @lightgrey1;
-  }
 </style>
