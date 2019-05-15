244e11e61ac1dcea23875063f86e82432f432b7a
354809232@qq.com <354809232@qq.com>
2019-05-15 14:36:29 +0800

    axios封装

--- src/Api/request.js
+++ src/Api/request.js
@@ -1,14 +1,9 @@
 // 所有请求放到这个文件中导出
 //存放请求方法
-import  axios from 'axios'
-import {api} from './api'
-let  getList = function () {
-  axios({
-    methods:'get',
-    url:api+'list'
-  }).then((data)=>{
+import sendAxios from './axios'
 
-  })
+let  getList = function () {
+  
 }
 
 export {
