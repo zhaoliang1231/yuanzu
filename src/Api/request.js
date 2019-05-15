9f7ae8a963352b34ffd0bf282f5ec3788ae1daa9
354809232@qq.com <354809232@qq.com>
2019-05-15 12:15:54 +0800

    安装axios 配置

--- src/Api/request.js
+++ src/Api/request.js
@@ -1,1 +1,18 @@
 // 所有请求放到这个文件中导出
+//存放请求方法
+import  axios from 'axios'
+import {api} from './api'
+let  getList = function () {
+  axios({
+    methods:'get',
+    url:api+'list'
+  }).then((data)=>{
+
+  })
+}
+
+export {
+  getList
+}
+
+
