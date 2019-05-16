2dc94432f65a821133b5cd6565ea90f1b54f49a5
chengeng666 <1021143798@qq.com>
2019-05-16 10:07:07 +0800

    会员中心

--- src/components/MemberCenter/index.vue
+++ src/components/MemberCenter/index.vue
@@ -1,5 +1,17 @@
 <template>
-<div>sdsadsa</div>
+<div class="memberCenter">
+  <div class="member-header">
+    <a>主页</a>
+    >
+    <a>我的账户</a>
+    >
+    <a>个人资料</a>
+  </div>
+  <div class="user-center">
+    <div class="user-nav">
+    </div>
+  </div>
+</div>
 </template>
 <script>
 export default {
@@ -8,7 +20,5 @@ export default {
 
 <style lang="less" scoped>
   @import "~static/css/common.less";
-div{
-  color: @pink;
-}
+  @import "~static/css/index.less";
 </style>
