d9d5eb90642079bfc134d8bfb3095541ea8178ed
354809232@qq.com <354809232@qq.com>
2019-05-17 09:02:14 +0800

    登录页面提交

--- src/components/LoginRegister/index.vue
+++ src/components/LoginRegister/index.vue
@@ -10,23 +10,35 @@
             <a class="a-hover-pink" href="">登录/注册</a>
         </span>
     </div>
-      <div class="Z_login_title">
+      <div class="login_title">
           <el-tabs v-model="activeName" @click="handleClick">
-              <el-tab-pane label="登录" name="first">登录</el-tab-pane>
-              <el-tab-pane label="注册" name="second">注册</el-tab-pane>
+              <el-tab-pane label="登录" name="first" class="login_tab">
+                  <Login></Login>
+              </el-tab-pane>
+              <el-tab-pane label="注册" name="second">
+                  <Register></Register>
+              </el-tab-pane>
+
           </el-tabs>
       </div>
   </div>
 </template>
 
 <script>
-export default {
+    import Login from './Login/Login'
+    import Register from './Register/Register'
+
+    export default {
 name: 'index',
  data () {
     return {
      activeName: 'first'
     }
  },
+     components:{
+      Login,
+      Register
+     },
  methods:{
   handleClick(tab, event) {
 
@@ -36,5 +48,26 @@ name: 'index',
 </script>
 
 <style scoped lang="less">
+@import "../../static/css/common";
+
+/deep/.el-tabs__nav{
+   margin-left: 500px;
+}
+/deep/.el-tabs__item.is-active{
+    color:#333;
+}
+/deep/.el-tabs__item:hover{
+    color: #333;
+}
+//选项卡移动线
+/deep/.el-tabs__active-bar{
+    border-bottom: 2px solid #848484;
+
+}
+/deep/.el-tabs__item{
+    font-size: 16px;
+    color: @fontColor;
+}
+
 
 </style>
